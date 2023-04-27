bfc5b216e9ce9832073639029c8b204bc0be923b
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-04-27 21:51:13 +0430

    Add checkStatus reducer for categories.

--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -1,13 +1,20 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  categories: ['Under construction'],
+  categories: [],
 };
 
 const categoriesSlice = createSlice({
   name: 'categories',
   initialState,
-  reducers: {},
+  reducers: {
+    checkStatus: (state, action) => {
+      state.categories = action
+        .payload === 'Under construction'
+        ? ['Under construction']
+        : state.categories;
+    },
+  },
 });
 
 export default categoriesSlice.reducer;
