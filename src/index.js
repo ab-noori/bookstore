3635b82d2ad5192d29432941a7111dbed590ffce
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-04-28 03:30:27 +0430

    Add redux provider to App.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './redux/store';
 import './index.scss';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
-    </Provider>
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
 );
