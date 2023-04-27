a8d832a25ae78328d29c1f7b1a45e7f58f6228b5
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-04-28 02:41:13 +0430

    Add initial state array for booksSlice.

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,7 +1,26 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  books: [],
+  books: [
+    {
+      item_id: 'item1',
+      title: 'The Great Gatsby',
+      author: 'John Smith',
+      category: 'Fiction',
+    },
+    {
+      item_id: 'item2',
+      title: 'Anna Karenina',
+      author: 'Leo Tolstoy',
+      category: 'Fiction',
+    },
+    {
+      item_id: 'item3',
+      title: 'The Selfish Gene',
+      author: 'Richard Dawkins',
+      category: 'Nonfiction',
+    },
+  ],
 };
 
 const booksSlice = createSlice({
@@ -13,6 +32,7 @@ const booksSlice = createSlice({
     },
     removeBook: (state, action) => {
       state.books = state.books.filter((book) => book.id !== action.payload);
+      state.books = state.books.filter((book) => book.item_id !== action.payload.item_id);
     },
   },
 });
