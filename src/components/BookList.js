ca6f8a9b41d114521c0bad7b4b87f4ea63ab66d8
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-04-28 02:43:13 +0430

    Display and add new books through the book list components.

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,17 +1,49 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import Book from './Book';
+import { useSelector, useDispatch } from 'react-redux';
+import { addBook, removeBook } from '../redux/books/booksSlice';
 
-const BookList = ({ books, onDelete }) => (
-  <div>
-    {books.map((book) => (
-      <Book key={book.id} book={book} onDelete={onDelete} />
-    ))}
-  </div>
-);
+const BookList = () => {
+  const { books } = useSelector((state) => state.books);
+  const dispatch = useDispatch();
 
-BookList.propTypes = {
-  books: PropTypes.string.isRequired,
-  onDelete: PropTypes.func.isRequired,
+  const handleAddBook = () => {
+    const newBook = {
+      id: `item${books.length + 1}`,
+      title: 'New Book',
+      author: 'Unknown Author',
+      category: 'Unknown Category',
+    };
+    dispatch(addBook(newBook));
+  };
+
+  const handleRemoveBook = (id) => {
+    const bookToRemove = books.find((book) => book.id === id);
+    dispatch(removeBook(bookToRemove));
+  };
+
+  return (
+    <div>
+      <ul>
+        {books.map((book) => (
+          <li key={book.id}>
+            <span>
+              {book.title}
+              &nbsp;
+              &nbsp;
+              by
+              &nbsp;
+              &nbsp;
+              {book.author}
+            </span>
+            &nbsp;
+            &nbsp;
+            <button type="button" onClick={() => handleRemoveBook(book.id)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+      <button type="button" onClick={handleAddBook}>Add Book</button>
+    </div>
+  );
 };
+
 export default BookList;
