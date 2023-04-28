047bfe38e576ccbbc8c93e56b9abccbf0fc4d621
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-04-28 16:21:21 +0430

    Add BookForm component to homepage.

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,38 +1,46 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const BookForm = ({ onAdd }) => {
+const BookForm = ({ handleAddBook }) => {
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
+  const [category, setCategory] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    onAdd({ title, author });
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newBook = {
+      item_id: `item${Math.random().toString(36).substring(7)}`,
+      title: title.trim(),
+      author: author.trim(),
+      category: category.trim(),
+    };
+    handleAddBook(newBook);
     setTitle('');
     setAuthor('');
+    setCategory('');
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Title"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Author"
-        value={author}
-        onChange={(e) => setAuthor(e.target.value)}
-      />
+      <label htmlFor="title">
+        Title:
+        <input type="text" id="title" value={title} onChange={(event) => setTitle(event.target.value)} required />
+      </label>
+      <label htmlFor="author">
+        Author:
+        <input type="text" id="author" value={author} onChange={(event) => setAuthor(event.target.value)} required />
+      </label>
+      <label htmlFor="category">
+        Category:
+        <input type="text" id="category" value={category} onChange={(event) => setCategory(event.target.value)} required />
+      </label>
       <button type="submit">Add Book</button>
     </form>
   );
 };
 
 BookForm.propTypes = {
-  onAdd: PropTypes.func.isRequired,
+  handleAddBook: PropTypes.func.isRequired,
 };
 
 export default BookForm;
